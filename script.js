317753df6fb89a3e425b55acfaa0efc2016012a4
Fullpotatoes <ludovic.pouet@gmail.com>
2025-01-07 15:18:31 +0100

    add event listener

---
+++ script.js
@@ -0,0 +1,8 @@
+const SEARCH_BTN = document.getElementById('search-btn')
+const USER_REQUEST = document.getElementById('search-bar')
+
+SEARCH_BTN.addEventListener('click', () => {
+    let USER_VALUE = USER_REQUEST.value 
+    let URL = `https://developer.mozilla.org/fr/search?q=${USER_VALUE}&locale=fr&locale=en-US`
+    window.open(URL,'_blank')
+})
