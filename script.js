a776a5aba86fa141f32c1af8c4920bd5f8093e8e
Fullpotatoes <ludovic.pouet@gmail.com>
2025-01-08 12:12:57 +0100

    history + optimisation

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const SEARCH_BTN = document.getElementById('search-btn')
 const USER_REQUEST = document.getElementById('search-bar')
+let history = []
+
+function openTab(query){
+    let URL = `https://developer.mozilla.org/fr/search?q=${query}&locale=fr&locale=en-US`
+    window.open(URL,'_blank')
+}
+
+function addToHistory(query){
+    history.push(USER_VALUE)
+    return history
+}
 
 SEARCH_BTN.addEventListener('click', () => {
-    let USER_VALUE = USER_REQUEST.value 
-    let URL = `https://developer.mozilla.org/fr/search?q=${USER_VALUE}&locale=fr&locale=en-US`
-    window.open(URL,'_blank')
+    let USER_VALUE = USER_REQUEST.value
+    openTab(USER_VALUE)
+    addToHistory(USER_VALUE)
 })
 
-USER_REQUEST.addEventListener('keydown', function(evenement) {
-    if (evenement.key === 'Enter') {
-        let USER_VALUE = USER_REQUEST.value 
-        let URL = `https://developer.mozilla.org/fr/search?q=${USER_VALUE}&locale=fr&locale=en-US`
-        window.open(URL,'_blank')
+USER_REQUEST.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        let USER_VALUE = USER_REQUEST.value
+        openTab(USER_VALUE)
+
     }})
