ca2987528483cd6e1624aa4a8f298e691443edb4
Joe19071991 <lauranove66@gmail.com>
2025-01-09 11:56:25 +0100

    add function createHistory

--- script.js
+++ script.js
@@ -1,46 +1,52 @@
-const SEARCH_BTN = document.getElementById('search-btn')
-const SEARCH_BAR = document.getElementById('search-bar')
-SEARCH_BAR.focus()
+const SEARCH_BTN = document.getElementById("search-btn");
+const SEARCH_BAR = document.getElementById("search-bar");
+SEARCH_BAR.focus();
 
-let history = JSON.parse(window.localStorage.getItem('history')) || []
+let history = JSON.parse(window.localStorage.getItem("history")) || [];
 
-function openTab(query){
-    let URL = `https://developer.mozilla.org/fr/search?q=${query}&locale=fr&locale=en-US`
-    window.open(URL,'_blank')
+function openTab(query) {
+  let URL = `https://developer.mozilla.org/fr/search?q=${query}&locale=fr&locale=en-US`;
+  window.open(URL, "_blank");
 }
 
-function addToHistory(query){
-    if (history.length >= 10){
-        history.shift()
-    }
+function addToHistory(query) {
+  if (history.length >= 10) {
+    history.shift();
+  }
 
-    history.push(query)
-    window.localStorage.setItem('history', JSON.stringify(history))
+  history.push(query);
+  window.localStorage.setItem("history", JSON.stringify(history));
 }
 
-function createHistory(){
-    const HISTORY_CONTAINER = document.getElementById('history-container')
+function createHistory() {
+  const HISTORY_CONTAINER = document.getElementById("history-container");
+  let ulElement = document.createElement("ul");
 
-    history.forEach(item => {
-        let link = document.createElement('a')
-        let url = `https://developer.mozilla.org/fr/search?q=${item}&locale=fr&locale=en-US`
-        link.setAttribute('href',url)
-        link.innerHTML = item
-        HISTORY_CONTAINER.appendChild(link)
-    });
+  history.forEach((item) => {
+    let link = document.createElement("a");
+    let url = `https://developer.mozilla.org/fr/search?q=${item}&locale=fr&locale=en-US`;
+    let liElement = document.createElement("li");
+    link.setAttribute("href", url);
+    link.setAttribute("target", "_blank");
+    link.innerHTML = item;
+    liElement.appendChild(link);
+    ulElement.appendChild(liElement);
+    HISTORY_CONTAINER.appendChild(ulElement);
+  });
 }
 
-function eventHandler(){
-    let USER_REQUEST = SEARCH_BAR.value
-    openTab(USER_REQUEST)
-    addToHistory(USER_REQUEST)
+function eventHandler() {
+  let USER_REQUEST = SEARCH_BAR.value;
+  openTab(USER_REQUEST);
+  addToHistory(USER_REQUEST);
 }
 
-SEARCH_BTN.addEventListener('click', eventHandler)
+SEARCH_BTN.addEventListener("click", eventHandler);
 
-SEARCH_BAR.addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
-        eventHandler()
-    }})
-    
-createHistory()
+SEARCH_BAR.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    eventHandler();
+  }
+});
+
+createHistory();
